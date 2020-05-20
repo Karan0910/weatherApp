70f684eb587ff00c33084a8cbc1486802a09dea0
Karan Sharma <95sharma.karan@gmail.com>
2020-05-20 01:23:50 -0400

    Added cities list and current city location

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.weatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
@@ -24,6 +25,9 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    private HashMap<String,ArrayList<String>> countrytoCity =new HashMap<>();
+    ArrayList<String> countryList = new ArrayList<>();
+    ArrayList<String> city = new ArrayList<>();
     ProgressDialog progressDoalog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,25 +35,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        readCitiesList();
+
 
         progressDoalog = new ProgressDialog(MainActivity.this);
         progressDoalog.setMessage("Loading....");
         progressDoalog.show();
 
+        readCitiesList();
 
-        ArrayList<String> list=new ArrayList<String>();
+        Location gpsLocation = appLocationService
+                .getLocation(LocationManager.GPS_PROVIDER);
+        if (gpsLocation != null) {
+            double latitude = gpsLocation.getLatitude();
+            double longitude = gpsLocation.getLongitude();
+            String result = "Latitude: " + gpsLocation.getLatitude() +
+                    " Longitude: " + gpsLocation.getLongitude();
+            tvAddress.setText(result);
 
-        String[] locales = Locale.getISOCountries();
-
-        for (String countryCode : locales) {
-
-            Locale obj = new Locale("", countryCode);
-
-            System.out.println("Country Name = " );
-            list.add(obj.getDisplayCountry());
-
-        }
+        Utils.getCityFromLocation();
 
         /*Create handle for the RetrofitInstance interface*/
         GetData service = RetrofitClientInstance.getRetrofitInstance().create(GetData.class);
@@ -92,30 +95,46 @@ public class MainActivity extends AppCompatActivity {
         return json;
     }
 
-    public String readCitiesList() {
+
+
+    public void readCitiesList() {
+
+
+        String[] isoCountryCodes = Locale.getISOCountries();
+        for (String countryCode : isoCountryCodes) {
+            Locale locale = new Locale("", countryCode);
+            countryList.add(locale.getDisplayName());
+            System.out.println(locale.getDisplayName());
+        }
+
         try {
-            JSONObject obj = new JSONObject(loadJSONFromAsset());
-            JSONArray m_jArry = obj.getJSONArray("formules");
-            ArrayList<HashMap<String, String>> formList = new ArrayList<HashMap<String, String>>();
-            HashMap<String, String> m_li;
 
-            for (int i = 0; i < m_jArry.length(); i++) {
-                JSONObject jo_inside = m_jArry.getJSONObject(i);
-                Log.d("Details-->", jo_inside.getString("formule"));
-                String formula_value = jo_inside.getString("formule");
-                String url_value = jo_inside.getString("url");
+            for (String countryName : countryList) {
 
-                //Add your values in your `ArrayList` as below:
-                m_li = new HashMap<String, String>();
-                m_li.put("formule", formula_value);
-                m_li.put("url", url_value);
+                JSONObject obj = new JSONObject(loadJSONFromAsset());
+                countryName= countryName.replace("&","and");
+                JSONArray countryListArray=null;
+                if(obj.has(countryName)) {
+                 countryListArray = obj.getJSONArray(countryName);
 
-                formList.add(m_li);
+                ArrayList<String> list =new ArrayList<>();
+                for (int i = 0; i < countryListArray.length(); i++) {
+                    String cityName = countryListArray.getString(i);
+                    Log.d("Details-->", cityName);
+                    city.add(cityName);
+                    list.add(cityName);
+
+                }
+                countrytoCity.put(countryName,list);
+                }
             }
+
         } catch (JSONException e) {
             e.printStackTrace();
         }
     }
+
+
 
 
 }
